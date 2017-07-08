212350d19a6195e4c64d29c8905391ae5b578b95
robachma <robertest16@gmail.com>
2017-07-08 16:52:13 -0700

    adding notify component

--- notify/src/app/app.module.ts
+++ notify/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NotifyComponent } from './notify/notify.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotifyComponent
   ],
   imports: [
     BrowserModule
