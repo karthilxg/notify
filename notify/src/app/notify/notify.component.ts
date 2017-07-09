a8965d606b4e67062cecdf2b443d5e4df6140101
robachma <robertest16@gmail.com>
2017-07-09 16:27:55 -0700

    initial component constructed

--- notify/src/app/notify/notify.component.ts
+++ notify/src/app/notify/notify.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotifyComponent implements OnInit {
 
-  constructor() { }
+	private date;
+	private notifications: any[];
+	private reminders: any[];
+	private assignedTasks: any[];
+
+  constructor() {   	
+    this.date =  new Date(); 
+    setInterval(() => {
+        this.date =  new Date();
+     }, 1000);
+  }  
 
   ngOnInit() {
+  	this.notifications = ['note1','note1','note1','note1','note1','note1','note1','note1','note1','note1','note1','note1','note1','note1','note1'];
+  	this.reminders = ['reminder','reminder','reminder'];
   }
+
 
 }
